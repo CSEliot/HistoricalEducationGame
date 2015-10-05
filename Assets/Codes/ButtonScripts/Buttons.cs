4b6d6e427d295dd3f388f9860ce0cb38e6427319
ecsit <ecsit@localhost>
2015-10-05 02:14:05 -0400

    Almost finished base navigation

---
+++ Assets/Codes/ButtonScripts/Buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+    public float NewSceneNum = 0;
+
+    void Update()
+    {
+        if (NewSceneNum != 0)
+        {
+            CallSceneChange(NewSceneNum);
+        }
+    }
+    public void CallSceneChange(float NewScene)
+    { 
+        GameObject.FindGameObjectWithTag("MenuController").
+            GetComponent<AllMenuNav>().ChangeSceneTo((int)NewScene);
+    }
+}
