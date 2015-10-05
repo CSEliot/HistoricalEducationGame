f1ec3153e3c729a9bb818cca7d0c43fd81aaa871
ecsit <ecsit39@gmail.com>
2015-10-05 02:14:05 -0400

    Almost finished base navigation

---
+++ Assets/Codes/AllMenuNav.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AllMenuNav : MonoBehaviour {
+
+	public int CurrentCanvasNum;
+
+	public GameObject[] Canvases;
+
+	// Use this for initialization
+	void Start () {
+	    //disables all canvases in case I was messing around in one.
+        //then enables the one I want to test in play mode via "CurrentCanvasNum"
+        //game will be exported at Canvas 0.
+        foreach (GameObject Canvas in Canvases)
+        {
+            Canvas.SetActive(false);
+        }
+        if (!Application.isEditor) { CurrentCanvasNum = 0; }
+        Canvases[CurrentCanvasNum].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void ChangeSceneTo(int MenuInt)
+    {
+        Canvases[CurrentCanvasNum].SetActive(false);
+        Canvases[MenuInt].SetActive(true);
+        CurrentCanvasNum = MenuInt;
+    }
+}
