c0ed404a165ea475de1d927bf34f6dcf24b0e7cc
AD\eliot2 <ecsit39@gmail.com>
2015-10-06 01:51:22 -0400

    Current Bug: Animations don't reset the CallNewScene float to 0 so returning to used scenes causes bug. Need to fix. Also added sound bank and Bryan's crappy SFX.

---
+++ Assets/Codes/ButtonScripts/CheckPasswordExport.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheckPasswordExport : MonoBehaviour {
+
+    public string password;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Enter()
+    {   
+        if (GameObject.FindGameObjectWithTag("PasswordField")
+            .GetComponent<Text>().text == "shoe")
+        {
+            GameObject.FindGameObjectWithTag("MenuController").
+            GetComponent<AllMenuNav>().ChangeSceneTo(1);
+        }
+    }
+}
---
+++ Assets/Codes/SoundEffectManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectManager : MonoBehaviour {
+
+	public AudioClip[] SFXBank;
+	public AudioSource SoundSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlaySound(int SFXNum){
+		SoundSource.PlayOneShot(SFXBank [SFXNum]);
+	}
+}
