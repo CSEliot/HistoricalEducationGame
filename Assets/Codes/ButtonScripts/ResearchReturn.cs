3c3fe95900c30cb47d32714f31948d5282e5ae42
ecsit <ecsit39@gmail.com>
2015-10-10 05:04:27 -0400

    Got most things workinggot research page working but need to find out way to get around looping issue when switching to overlay scene

---
+++ Assets/Codes/ButtonScripts/ResearchReturn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResearchReturn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Return()
+    {
+        GameObject.Find("5 - ResearchGame").SetActive(false);
+    }
+}
