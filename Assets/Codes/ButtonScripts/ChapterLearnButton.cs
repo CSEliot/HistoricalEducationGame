38ba4315256492b034a13c0f6a72dc2716bfa356
ecsit <ecsit39@gmail.com>
2015-10-11 04:24:40 -0400

    Research Screen complete

---
+++ Assets/Codes/ButtonScripts/ChapterLearnButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChapterLearnButton : MonoBehaviour {
+
+    public int ChapterNum;
+    public AllChapterButtons ButtonObjectScript;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ActivateButton()
+    {
+        int totalChapters = 7;
+        for (int i = 0; i < totalChapters; i++){
+            //Disable all first.
+            ButtonObjectScript.AllChapters[i].SetActive(false);
+        }
+        ButtonObjectScript.AllChapters[ChapterNum-1].SetActive(true);
+    }
+}
