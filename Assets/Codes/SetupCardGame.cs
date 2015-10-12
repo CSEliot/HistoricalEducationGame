a752bcc6e0d8576e2a62358bc284aadb8dd72f45
ecsit <ecsit39@gmail.com>
2015-10-12 02:39:53 -0400

    Completed layout. Now begins all gameplay

---
+++ Assets/Codes/SetupCardGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetupCardGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame(int StageNum) {
+        GameObject.FindGameObjectWithTag("MenuController").GetComponent<AllMenuNav>().ChangeSceneTo(4);
+    }
+}
