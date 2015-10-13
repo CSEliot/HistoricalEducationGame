2ff50178f83ac7a6cdadcbf5bce105f251905a0f
AD\eliot2 <ecsit39@gmail.com>
2015-10-13 17:27:51 -0400

    Card Support Engaged!

---
+++ Assets/Codes/AllSpecialFunctions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class AllSpecialFunctions : MonoBehaviour {
+
+	public Dictionary<int, Action> SpecialAbilityList; 
+
+	// Use this for initialization
+	void Start () {
+		SpecialAbilityList = new Dictionary<int, Action> ();
+		SpecialAbilityList.Add(1, ()=>TestMe());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TestMe(){
+		print ("pwep");
+	}
+}
---
+++ Assets/Codes/Card.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+public class Card : MonoBehaviour {
+
+	private int Influence;
+	private string Name;
+	private string FlavorText;
+	private Image Graphic;
+	private bool IsSpecial;
+	private delegate void PrintyFunction();
+	Action NewFunction;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NewFunction = GameObject.Find ("Main Camera").GetComponent<AllSpecialFunctions> ().SpecialAbilityList [1];
+		NewFunction ();
+	}
+
+
+}
