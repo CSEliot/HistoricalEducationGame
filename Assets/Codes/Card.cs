778a5f73fee156cbfad1403824bcd86d36d07e4a
ecsit <ecsit39@gmail.com>
2015-10-14 01:56:34 -0400

    Started implementing card generation

--- Assets/Codes/Card.cs
+++ Assets/Codes/Card.cs
@@ -10,17 +10,22 @@ public class Card : MonoBehaviour {
 	private Image Graphic;
 	private bool IsSpecial;
 	private delegate void PrintyFunction();
-	Action NewFunction;
+	private Action SpecialAbility;
 
 	// Use this for initialization
 	void Start () {
+        if(IsSpecial)
+		SpecialAbility = GameObject.Find ("Main Camera").GetComponent<AllSpecialFunctions> ().SpecialAbilityList [1];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		NewFunction = GameObject.Find ("Main Camera").GetComponent<AllSpecialFunctions> ().SpecialAbilityList [1];
-		NewFunction ();
 	}
+
+    public void AssignData(int Influence, string Name, string FlavorText)
+    {
+
+    }
 
 
 }
