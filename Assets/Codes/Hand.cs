c27b4eefe1e3cd81e2084409c556562644f43692
AD\eliot2 <umbc.edu@localhost>
2015-10-17 03:38:21 -0400

    Beginning game setup

---
+++ Assets/Codes/Hand.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hand : MonoBehaviour {
+
+	private List<Card> Cards;
+	private int HandSize;
+	private Deck deck;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame(){
+		Cards = new List<Card> (5);
+
+	}
+
+	public void DrawCard(Card NewCard){
+
+		Cards.Add (NewCard);
+	}
+
+	public void PlayCard(int ChosenCardNum){
+		//Animation Calls
+		//Put card in Field list. (C# defaults to PbV)
+		//Remove card from Hand list.
+	}
+}
---
+++ Assets/Codes/TurnManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnManager : MonoBehaviour {
+
+	public Hand PlayerHand;
+	public Hand AIHand;
+
+	private enum Turn{
+		FillingHand,
+		ChoosingCard,
+		ActivatingAbilities,
+		SwitchingTurn,
+		Waiting
+	};
+
+	private Turn TurnState; 
+	// Use this for initialization
+	void Start () {
+		TurnState = Turn.FillingHand;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(TurnState == Turn.FillingHand){
+			//StartCoroutine("PlayerDrawCard");
+			TurnState = Turn.Waiting;
+		}
+	}
+
+	IEnumerator PlayerDrawCard(){
+
+		TurnState = Turn.ChoosingCard;
+		yield return null;
+	}
+
+	private void WaitForCardChoice(){
+	}
+
+	private void EndTurn(){
+	}
+
+	private void ActivateCardAbilties(){
+	}
+
+
+}
