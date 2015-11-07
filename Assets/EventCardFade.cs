5dc08f17ed172710f34b9a69a39bfb662dfa6758
AD\eliot2 <ecsit39@gmail.com>
2015-11-07 04:01:40 -0500

    Card choice is mid-implement. Should I recreate the "cardspotchosen" and "handplayed' functions ??? or . . .

---
+++ Assets/EventCardFade.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class EventCardFade : MonoBehaviour {
+
+
+    public Material myMat;
+    private bool beginFading;
+    private float myAlpha;
+    
+    // Use this for initialization
+    void Start () {
+        myAlpha = 1f;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (beginFading)
+        {
+            FadeMaterial();
+        }
+    }
+
+    public void FadeObj(GameObject fadeCard)
+    {
+        fadeCard.transform.GetChild(0).GetComponent<Image>().material = myMat;
+        fadeCard.transform.GetChild(1).GetComponent<Image>().material = myMat;
+        fadeCard.transform.GetChild(3).GetComponent<Image>().material = myMat;
+        float FadeTime = 2f;
+        fadeCard.transform.SetParent(transform, false);
+        StartCoroutine(DestroyIn(FadeTime, fadeCard));
+        beginFading = true;
+    }
+
+    private void FadeMaterial()
+    {
+        myAlpha = Mathf.Lerp(myAlpha, 0f, Time.deltaTime);
+        myMat.SetColor("_Color", new Color(1f, 1f, 1f, myAlpha));
+        Debug.Log("My alpha: " + myAlpha);
+    }
+
+    IEnumerator DestroyIn(float seconds, GameObject fadeCard)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(fadeCard);
+        myAlpha = 1f;
+        beginFading = false;
+    }
+}
